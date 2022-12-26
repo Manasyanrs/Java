8987b2785af165a60399a7e9adc5de2a73952dd2
radik <radikmanasyan@gmail.com>
2022-12-26 20:03:08 +0400

    added homework to ArrayUtil class and added chapter 4 examples

--- src/homework/homework2/ArrayUtil.java
+++ src/homework/homework2/ArrayUtil.java
@@ -5,18 +5,57 @@ public class ArrayUtil {
         int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30, 88, 1};
         int max_digit = array[0];
         int min_digit = array[0];
-        System.out.println("All array elements");
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > max_digit) {
-                max_digit = array[i];
-            }
-            if (array[i] < min_digit) {
-                min_digit = array[i];
-            }
+        int digit;
+        System.out.print("All array elements: ");
+        for (int i = 1; i < array.length; i++) {
+            max_digit = array[i] > max_digit ? array[i] : max_digit;
+            min_digit = array[i] < min_digit ? array[i] : min_digit;
             System.out.print(array[i] + ", ");
         }
         System.out.println();
         System.out.println("\nThe max digit in the array " + max_digit);
-        System.out.println("\nThe min digit in the array " + min_digit);
+        System.out.println("The min digit in the array " + min_digit);
+
+        System.out.print("\nEven number(s): ");
+        int count_event_numbers = 0;
+        for (int i = 0; i < array.length; i++) {
+            digit = array[i] % 2;
+            if (digit == 0) {
+                System.out.print(array[i] + ", ");
+                count_event_numbers++;
+            }
+
+        }
+
+        System.out.print("\nOdd number(s): ");
+        int count_odd_numbers = 0;
+        int total_sum_array_elements = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            total_sum_array_elements += array[i];
+            digit = array[i] % 2;
+            if (digit == 1) {
+                System.out.print(array[i] + ", ");
+                count_odd_numbers++;
+            }
+        }
+        System.out.println("\nThe number of even number(s) in the array is " + count_event_numbers);
+        System.out.println("The number of odd number(s) in the array is " + count_odd_numbers);
+        int array_average_value = total_sum_array_elements / array.length;
+        System.out.println("\nArray average value = " + array_average_value);
+        System.out.println("Total sum of the array elements = " + total_sum_array_elements);
+        System.out.println("\nFirs element of the array: " + array[0]);
+        System.out.println("Array end element: " + array[array.length - 1]);
+        if (array.length % 2 == 0) {
+            int middle_elements[] = new int[2];
+            for (int i = 0; i < 2; i++) {
+                middle_elements[i] = array[(array.length - 1) / 2 + i];
+            }
+            System.out.println("Middle element of the array: " + middle_elements[0] + ", " + middle_elements[1]);
+
+        } else {
+            System.out.println("Middle element of the array: " + array[array.length / 2]);
+
+        }
     }
 }
