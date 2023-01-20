bdc16ab371405b6aa965590036f952efd97faa87
radik <radikmanasyan@gmail.com>
2023-01-20 23:36:32 +0400

    Chapter 7 examples

---
+++ src/chapter7/OverloadDemo1.java
@@ -0,0 +1,13 @@
+package chapter7;
+
+public class OverloadDemo1 {
+    void test(){
+        System.out.println("Пapaмeтpы отсутствуют");
+    }
+    void test(int a, int b){
+        System.out.println("a и b: " + a + " " + b);
+    }
+    void test(double a){
+        System.out.println("Bнyтpeннee преобразование при вызове test(double) a: " + a);
+    }
+}
---
+++ src/chapter7/Test2.java
@@ -0,0 +1,21 @@
+package chapter7;
+
+public class Test2 {
+    int a;
+    int b;
+
+    Test2(int i, int j) {
+        a = i;
+        b = j;
+    }
+
+    void meth(int i, int j) {
+        i *= 2;
+        j /= 2;
+    }
+
+    void meth(Test2 o) {
+        o.a *= 2;
+        o.b /= 2;
+    }
+}
