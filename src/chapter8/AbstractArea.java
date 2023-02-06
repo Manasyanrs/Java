b7bf216517127c5ed8c59c8cd95a0ce477ce4fa7
Manasyanrs <radikmanasyan@gmail.com>
2023-02-06 17:14:04 +0400

    Have copied all book examples to chapter 8

---
+++ src/chapter8/AbstractArea.java
@@ -0,0 +1,15 @@
+package chapter8;
+
+public class AbstractArea {
+    public static void main(String[] args) {
+        RectangleF r = new RectangleF(9, 5);
+        TriangleF t = new TriangleF(10, 8);
+        AbstractFigure figref;
+
+        figref = r;
+        System.out.println("Плoщaдь равн " + figref.area());
+        figref = t;
+        System.out.println("Плoщaдь равн " + figref.area());
+
+    }
+}
