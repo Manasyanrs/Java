602b348cfefa8d03b440f903cb065c8649eef171
Manasyanrs <radikmanasyan@gmail.com>
2023-02-09 14:14:20 +0400

    Have changed switch cases, moved cases logic to the methods

--- src/homework/employee/EmployeeStorage.java
+++ src/homework/employee/EmployeeStorage.java
@@ -1,10 +1,35 @@
 package homework.employee;
 
+import java.util.Scanner;
+
 public class EmployeeStorage {
+    Scanner console = new Scanner(System.in);
     private Employee[] employees = new Employee[2];
     private int size = 0;
+    boolean userID = true;
 
-    public void addEmployee(Employee value) {
+    public void unitTestEmployee() {
+        Employee user1 = new Employee("User1", "surName", "1",
+                123.3, "TestUser", "QA");
+        addEmployee(user1);
+        Employee user2 = new Employee("user2", "user2SurName", "16",
+                12345, "Ak47", "Constructor");
+        addEmployee(user2);
+    }
+
+    public String startCommands() {
+        System.out.print("\nPress 0 for exit.\n" +
+                "Press 1 add employee.\n" +
+                "Press 2 print all employees.\n" +
+                "Press 3 search employee by employee ID.\n" +
+                "Press 4 search employee by company name.\n" +
+                "Make a choice: ");
+        String command = console.nextLine();
+        System.out.println();
+        return command;
+    }
+
+    private void addEmployee(Employee value) {
         if (size <= employees.length - 1) {
             employees[size] = value;
             size++;
@@ -13,6 +38,43 @@ public class EmployeeStorage {
             extend();
             addEmployee(value);
         }
+    }
+
+    public void createEmployee() {
+        System.out.print("Please enter the name: ");
+        String name = console.nextLine();
+
+        System.out.print("Please enter the surName: ");
+        String surName = console.nextLine();
+
+        System.out.print("Please enter the employeeID: ");
+        String employeeID = console.nextLine();
+
+        if (isEmployeeID(employeeID)) {
+            while (userID) {
+                System.out.println("Sorry but this employeeID already exists.");
+                System.out.print("Please change the employeeID: ");
+                String changeEmployeeID = console.nextLine();
+                userID = isEmployeeID(changeEmployeeID);
+                if (!userID) {
+                    employeeID = changeEmployeeID;
+                }
+            }
+        }
+
+        System.out.print("Please enter the salary: ");
+        String salary = console.nextLine();
+
+        System.out.print("Please enter the company: ");
+        String company = console.nextLine();
+
+        System.out.print("Please enter the position: ");
+        String position = console.nextLine();
+
+        Employee employee = new Employee(name, surName, employeeID,
+                Double.parseDouble(salary), company, position);
+        addEmployee(employee);
+        System.out.println("\nUser created and add to data base.");
     }
 
     private void extend() {
@@ -29,7 +91,7 @@ public class EmployeeStorage {
         }
     }
 
-    public boolean isEmployeeID(String employeeID) {
+    private boolean isEmployeeID(String employeeID) {
         for (int i = 0; i < size; i++) {
             if (employees[i].getEmployeeID().equals(employeeID)) {
                 return true;
@@ -38,23 +100,27 @@ public class EmployeeStorage {
         return false;
     }
 
-    public void searchEmployeeByEmployeeID(String id) {
+    public Employee searchEmployeeByEmployeeID() {
+        System.out.print("Please enter the employeeID: ");
+        String searchEmployeeByID = console.nextLine();
         for (int i = 0; i < size; i++) {
-            if (employees[i].getEmployeeID().equals(id)) {
-                System.out.println(employees[i].toString());
-                return;
+            if (employees[i].getEmployeeID().equals(searchEmployeeByID)) {
+                return employees[i];
             }
         }
-        System.out.println("Employee by id " + id + " was not found.");
+        System.out.print("Employee by id " + searchEmployeeByID + " was not found. ");
+        return null;
     }
 
-    public void searchEmployeeByCompanyName(String companyName) {
+    public Employee searchEmployeeByCompanyName() {
+        System.out.print("Please enter the employeeID: ");
+        String searchEmployeeByCompanyName = console.nextLine();
         for (int i = 0; i < size; i++) {
-            if (employees[i].getCompany().equals(companyName)) {
-                System.out.println(employees[i]);
-                return;
+            if (employees[i].getCompany().equals(searchEmployeeByCompanyName)) {
+                return employees[i];
             }
         }
-        System.out.println("Company by name " + companyName + " was not found.");
+        System.out.print("Company by name " + searchEmployeeByCompanyName + " was not found. ");
+        return null;
     }
 }
