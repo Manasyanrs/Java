a1a15514460b10cb955dbaf15915836a2232e4fe
Manasyanrs <radikmanasyan@gmail.com>
2023-02-16 13:46:10 +0400

    Have copied book examples to the package chapter9

--- src/chapter9/IntStack.java
+++ src/chapter9/IntStack.java
@@ -4,4 +4,25 @@ public interface IntStack {
     void push(int item);
 
     int pop();
+
+    default void clear() {
+        System.out.println("Meтoд clear() не реализован.");
+    }
+
+    default int[] popNElements(int n) {
+        return getElements(n);
+    }
+
+    default int[] skipAndPopElements(int skip, int n) {
+        getElements(skip);
+        return getElements(n);
+    }
+
+    private int[] getElements(int n) {
+        int[] elements = new int[n];
+        for (int i = 0; i < n; i++) {
+            elements[i] = pop();
+        }
+        return elements;
+    }
 }
---
+++ src/chapter9/MyIfImp2.java
@@ -0,0 +1,11 @@
+package chapter9;
+
+public class MyIfImp2 implements MyIf {
+    public int getNumber() {
+        return 100;
+    }
+
+    public String getString() {
+        return "Это другая символьная строка.";
+    }
+}
