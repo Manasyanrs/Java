247d8bc0a0d321d6724b95234762cda886c22540
Manasyanrs <radikmanasyan@gmail.com>
2023-03-12 00:30:31 +0400

    generics list in java

---
+++ src/homework/homework11/CollectionExample1.java
@@ -0,0 +1,51 @@
+package homework.homework11;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CollectionExample1 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        List<String> listNames = new ArrayList<>();
+        listNames.add("Tom");
+        listNames.add("Mary");
+        listNames.add("Mark");
+        listNames.add("Adam");
+        listNames.add("Eva");
+
+        if (isNameInNamesList(listNames)) {
+            System.out.println("Name is on the list.");
+        } else {
+            System.out.println("Name is not on the list.");
+        }
+
+        deleteNameIfNameInList(listNames);
+
+    }
+
+    public static boolean isNameInNamesList(List<String> listNames) {
+        System.out.print("Input checked name: ");
+        String strName = scanner.nextLine();
+        return listNames.contains(strName);
+    }
+
+    public static void deleteNameIfNameInList(List<String> listNames) {
+        System.out.println("\n" + listNames);
+        System.out.print("Which name do you want to delete? ");
+        String strName = scanner.nextLine();
+        if (listNames.contains(strName)) {
+            listNames.remove(strName);
+            System.out.println(listNames);
+            System.out.println(strName + " deleted in names list.");
+        }else {
+            System.out.println(strName + " is not on the list.");
+
+        }
+    }
+
+//   TODO Ես միկը հասկացա բայց իրականացում չկարացա տաի
+//    Գրեք կոդ, որ մարդը ներմուծի անուն, ու ինչ որ ինդեքս, դուք էդ ինդեքսի տակ տանեք էդ անունը դնեք.
+}
---
+++ src/homework/homework11/CollectionExample2.java
@@ -0,0 +1,48 @@
+package homework.homework11;
+
+import java.util.*;
+
+import java.util.Collections;
+
+public class CollectionExample2 {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final List<String> namesList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        addNames(5);
+        printUniqueNamesFromList();
+        System.out.println();
+        addNamesInListAndPrintAllSortedNamesFromList(10);
+    }
+
+    static void addNames(int enterCountInputNames) {
+        System.out.println();
+        for (int i = 0; i < enterCountInputNames; i++) {
+            System.out.print("Input " + (i + 1) + " name: ");
+            String inputName = scanner.nextLine();
+            namesList.add(inputName);
+        }
+    }
+
+    static void printUniqueNamesFromList() {
+        Set<String> genericNamesList = new TreeSet<>(getNamesList());
+        System.out.println(genericNamesList);
+    }
+
+    static void addNamesInListAndPrintAllSortedNamesFromList(int enterCountInputNames) {
+        List<String> sortedNamesList = new LinkedList<>();
+
+        for (int i = 0; i < enterCountInputNames; i++) {
+            System.out.print("Input " + (i + 1) + " name: ");
+            String inputName = scanner.nextLine();
+            sortedNamesList.add(inputName);
+        }
+        Collections.sort(sortedNamesList);
+        System.out.println(sortedNamesList);
+    }
+
+    public static List<String> getNamesList() {
+        return namesList;
+    }
+}
